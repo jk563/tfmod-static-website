641d020f4b50f09cc889694dd7e1a57e7785010d
Jamie Kelly <git@jamiekelly.com>
2020-02-27 22:43:22 +0000

    Basic bucket

---
+++ s3_bucket-main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.subdomain}.${var.domain}"
+  acl    = "public-read"
+
+	website {
+		index_document = var.entrypoint
+	}
+}
+
--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "subdomain" {
+	description = "The subdomain of the bucket"
+	type = string
+	default = "www"
+}
 
+variable "domain" {
+	description = "The root domain of the bucket"
+	type = string
+}
+
+variable "entrypoint" {
+	description = "The homepage of the website"
+	type = string
+	default = "index.html"
+}
