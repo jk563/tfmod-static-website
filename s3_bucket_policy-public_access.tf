460962f82021d6f55275a03666a0a1feed15d736
Jamie Kelly <git@jamiekelly.com>
2020-02-27 23:18:49 +0000

    Public bucket access

---
+++ s3_bucket_policy-public_access.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket_policy" "main_public_access" {
+  bucket = aws_s3_bucket.main.id
+	policy = data.aws_iam_policy_document.public_access.json
+}
+
+data "aws_iam_policy_document" "public_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.main.id}/*",
+    ]
+  }
+}
